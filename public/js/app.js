346c2680c9a1b2bba728e432aeaaf9e614373308
runyud <runyud@umich.edu>
2021-06-03 20:06:17 -0400

    setup for deployment:

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     locationMsg.textContent = 'Loading...'
     forecast.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then ((response) => {
+    fetch('/weather?address=' + location).then ((response) => {
         response.json().then((data) => {
             if(data.error) {
                 locationMsg.textContent = data.error
